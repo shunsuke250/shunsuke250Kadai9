822ba9d017c0afe2300287e7912afdea4b801a81
shunsuke250 <soeyamashunsuke@gmail.com>
2023-09-24 15:49:25 +0900

    :sparkles: プロジェクトを作成

---
+++ PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrefecturesModalApp23-09-24
+//
+//  Created by 副山俊輔 on 2023/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

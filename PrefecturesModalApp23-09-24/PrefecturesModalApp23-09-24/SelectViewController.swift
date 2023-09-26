7a42efcd98dc60597063f1e4b6446056b607a8d6
shunsuke250 <soeyamashunsuke@gmail.com>
2023-09-26 22:11:13 +0900

    :sparkles: 都道府県を選択する画面のファイルを追加

---
+++ PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/SelectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectViewController.swift
+//  PrefecturesModalApp23-09-24
+//
+//  Created by 副山俊輔 on 2023/09/26.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

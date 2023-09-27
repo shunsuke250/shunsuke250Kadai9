86e25b3042319062e400a97a58a641b796c855b7
shunsuke250 <soeyamashunsuke@gmail.com>
2023-09-27 20:00:01 +0900

    :sparkles: 選択した都道府県を表示する機能を追加

--- PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/SelectViewController.swift
+++ PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/SelectViewController.swift
@@ -9,25 +9,45 @@ import UIKit
 
 class SelectViewController: UIViewController {
 
-    @IBOutlet weak var tokyoButton: UIButton!
-    @IBOutlet weak var kanagawaButton: UIButton!
-    @IBOutlet weak var saitamaButton: UIButton!
-    @IBOutlet weak var chibaButton: UIButton!
-
+    @IBOutlet private weak var tokyoButton: UIButton!
+    @IBOutlet private weak var kanagawaButton: UIButton!
+    @IBOutlet private weak var saitamaButton: UIButton!
+    @IBOutlet private weak var chibaButton: UIButton!
+    
+    var selectedPrefecture: String?
+    
     @IBAction func didTapTokyoButton(_ sender: Any) {
-    }
-    
-    @IBAction func didTapKanagawaButton(_ sender: Any) {
-    }
-    
-    @IBAction func didTapSaitamaButton(_ sender: Any) {
-    }
-    
-    @IBAction func didTapChibaButton(_ sender: Any) {
-    }
-
-    @IBAction func didTapCancelButton(_ sender: Any) {
+        selectedPrefecture = "東京都"
+        updateSelectedPrefectureLabel()
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func didTapKanagawaButton(_ sender: Any) {
+        selectedPrefecture = "神奈川県"
+        updateSelectedPrefectureLabel()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapSaitamaButton(_ sender: Any) {
+        selectedPrefecture = "埼玉県"
+        updateSelectedPrefectureLabel()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapChibaButton(_ sender: Any) {
+        selectedPrefecture = "千葉県"
+        updateSelectedPrefectureLabel()
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func didTapCancelButton(_ sender: Any) {
+        updateSelectedPrefectureLabel()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func updateSelectedPrefectureLabel() {
+        if let selectedPrefecture = selectedPrefecture, let viewController = presentingViewController as? ViewController {
+            viewController.selectedPrefectureLabel.text = selectedPrefecture
+        }
+    }
 }

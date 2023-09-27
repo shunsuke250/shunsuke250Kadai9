b75fb0f0a6b13389bfdce2fde486e2e9c5292d6f
shunsuke250 <soeyamashunsuke@gmail.com>
2023-09-27 20:09:00 +0900

    :+1: 軽微な修正

--- PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/SelectViewController.swift
+++ PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/SelectViewController.swift
@@ -13,27 +13,27 @@ class SelectViewController: UIViewController {
     @IBOutlet private weak var kanagawaButton: UIButton!
     @IBOutlet private weak var saitamaButton: UIButton!
     @IBOutlet private weak var chibaButton: UIButton!
-    
+
     var selectedPrefecture: String?
-    
+
     @IBAction func didTapTokyoButton(_ sender: Any) {
         selectedPrefecture = "東京都"
         updateSelectedPrefectureLabel()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func didTapKanagawaButton(_ sender: Any) {
         selectedPrefecture = "神奈川県"
         updateSelectedPrefectureLabel()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func didTapSaitamaButton(_ sender: Any) {
         selectedPrefecture = "埼玉県"
         updateSelectedPrefectureLabel()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func didTapChibaButton(_ sender: Any) {
         selectedPrefecture = "千葉県"
         updateSelectedPrefectureLabel()
@@ -44,7 +44,7 @@ class SelectViewController: UIViewController {
         updateSelectedPrefectureLabel()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     func updateSelectedPrefectureLabel() {
         if let selectedPrefecture = selectedPrefecture, let viewController = presentingViewController as? ViewController {
             viewController.selectedPrefectureLabel.text = selectedPrefecture

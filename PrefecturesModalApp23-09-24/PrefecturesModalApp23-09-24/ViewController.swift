1750e596775b8a606b9db70971c891ee67c6866c
akio0911 <akio0911@gmail.com>
2023-10-04 08:47:09 +0900

    feedback

--- PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/ViewController.swift
+++ PrefecturesModalApp23-09-24/PrefecturesModalApp23-09-24/ViewController.swift
@@ -10,24 +10,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var selectedPrefectureLabel: UILabel!
-    @IBOutlet private weak var selectButton: UIButton!
-    
-    // ViewController内で選択された都道府県を保持するプロパティ
-    var selectedPrefecture = ""
 
     @IBAction func didTapSelectButton(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let selectViewController = storyboard.instantiateViewController(withIdentifier: "SelectViewController") as? SelectViewController {
             selectViewController.delegate = self
             selectViewController.modalPresentationStyle = .fullScreen
-            self.present(selectViewController, animated: true, completion: nil)
+            present(selectViewController, animated: true, completion: nil)
         }
     }
 }
 
 extension ViewController: SelectViewControllerDelegate {
     func didSelectPrefecture(_ selectedPrefecture: String) {
-        self.selectedPrefecture = selectedPrefecture
         selectedPrefectureLabel.text = selectedPrefecture
     }
 }
